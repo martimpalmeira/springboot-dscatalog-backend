811a1a3084088e26a76d530adc619494cfed652f
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-07-23 03:22:54 -0300

    Creating backend project

---
+++ backend/src/main/java/com/devsuperior/dscatolog/DscatologApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatologApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscatolog/DscatologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
