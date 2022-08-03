3f324a4ce31ca41b59a0dff5ef8ebaceb47f165b
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-08-03 11:45:47 -0300

    custom create validation

--- backend/src/main/java/com/devsuperior/dscatolog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatolog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dscatolog.dto;
 
+import com.devsuperior.dscatolog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
 	private static final long serialVersionUID = 1L;
